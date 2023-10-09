36ceb87052b1514d6799f3f8d8c84475fe17b2f4
balla-sravani <137860965+balla-sravani@users.noreply.github.com>
2023-10-09 19:50:10 +0530

    Delete 07-10-2023/def bs(l,si,li,se,ceil):     if si<=li:         mid=(si+li) directory

--- 07-10-2023/def bs(l,si,li,se,ceil):     if si<=li:         mid=(si+li)/palindromeornot.py
+++
@@ -1,7 +0,0 @@
-s=input()
-s1=s[::-1]
-if s==s1:
-    print("yes")
-else:
-    print("no")
-    
--- 07-10-2023/def bs(l,si,li,se,ceil):     if si<=li:         mid=(si+li)/sepatator.py
+++
@@ -1,8 +0,0 @@
-# Function to separate digits of a number in a list
-def separate_digits(number):
-    return [int(digit) for digit in str(number)]
-
-# Example usage
-input_number = 10
-result = separate_digits(input_number)
-print(result)
